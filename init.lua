861baf824f62345453248a581e25e7be7303169e
Li0n <li0nsdickachu@outlook.com>
2021-03-17 22:45:19 +0100

    add gears, cracky 3 to 1
    register red and green gear nodes, and change cracky value of floorpiece from 3 to 1, and edit gear texture to stand out more

--- init.lua
+++ init.lua
@@ -1,6 +1,38 @@
 minetest.register_node("roborally:floorpiece", {
     description = "Floorpiece",
     walkable = true,
-    groups = {cracky = 3, level = 2},
-    tiles = {"floorpiece.png"}
+    groups = {cracky = 1, level = 2},
+    tiles = {"floorpiece.png",}
+})
+
+minetest.register_node("roborally:redgear", {
+    description = "Red Gear",
+    drawtype = "signlike",
+    groups = {cracky = 1, level = 2},
+    tiles = {"gear_red.png"},
+    is_ground_content = false,
+    paramtype = "light",
+    sunlight_propagates = false,
+    paramtype2 = "wallmounted",
+    walkable = false,
+    inventory_image = "gear_red.png",
+    selection_box = {
+        type = "wallmounted",
+    }
+})
+
+minetest.register_node("roborally:greengear", {
+    description = "Green Gear",
+    drawtype = "signlike",
+    groups = {cracky = 1, level = 2},
+    tiles = {"gear_green.png"},
+    is_ground_content = false,
+    paramtype = "light",
+    sunlight_propagates = false,
+    paramtype2 = "wallmounted",
+    walkable = false,
+    inventory_image = "gear_green.png",
+    selection_box = {
+        type = "wallmounted",
+    }
 })
